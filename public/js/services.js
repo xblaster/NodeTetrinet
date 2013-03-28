d46954dc5d5137a355babc90090b677cd3dd25fe
Jerome Wax <jerome.wax@lo2k.net>
2013-03-28 10:39:26 +0100

    code cleanup

--- public/js/services.js
+++ public/js/services.js
@@ -1,13 +1,9 @@
-var briceUrl = "http://192.168.1.107\\:8080/w3e/services/" + 'beneficiary/list';
-var myUrl = "http://localhost\\:8888/poc-raiffeisen/beneficiaires.json";
-var defaultPrefix = myUrl;
-
 angular.module('pocServices', ['ngResource']).factory('Beneficiary', function($resource) {
-	return $resource(defaultPrefix, {}, {
+	/*return $resource(defaultPrefix, {}, {
 		query : {
 			method : 'GET', isArray : true, params : {}
 		}
-	});
+	});*/
 }); 
 
 
